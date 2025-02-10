12a096ae0fe2e51cd2b7beb01d2b3afb51d406e2
Hans-J. Schmid <knock@myopendoor.de>
2025-02-11 00:48:21 +0100

    game version #1

--- game/version1/game/load.py
+++ game/version1/game/load.py
@@ -1,25 +1,35 @@
-import pyglet
-import random
 import math
+from random import randint
+
+from pyglet.sprite import Sprite
+
 from . import resources
 
+Point = tuple[float, float, float]
 
-def distance(point_1=(0, 0), point_2=(0, 0)):
+
+def distance(
+    point_1: Point = (0, 0, 0),
+    point_2: Point = (0, 0, 0),
+):
     """Returns the distance between two points"""
     return math.sqrt((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
 
 
-def asteroids(num_asteroids, player_position):
-    """Generate asteroid objects with random positions and velocities, 
-    not close to the player"""
-    asteroids = []
-    for i in range(num_asteroids):
+def asteroids(num_asteroids: int, player_position: Point) -> list[Sprite]:
+    """Generate asteroid objects with random positions and velocities, not too close to the player"""
+    asteroids: list[Sprite] = []
+    for _ in range(num_asteroids):
         asteroid_x, asteroid_y, _ = player_position
-        while distance((asteroid_x, asteroid_y), player_position) < 100:
-            asteroid_x = random.randint(0, 800)
-            asteroid_y = random.randint(0, 600)
-        new_asteroid = pyglet.sprite.Sprite(img=resources.asteroid_image,
-                                            x=asteroid_x, y=asteroid_y)
-        new_asteroid.rotation = random.randint(0, 360)
+        while distance((asteroid_x, asteroid_y, _), player_position) < 100:
+            asteroid_x = randint(0, 800)
+            asteroid_y = randint(0, 600)
+
+        new_asteroid = Sprite(
+            img=resources.asteroid_image,
+            x=asteroid_x,
+            y=asteroid_y,
+        )
+        new_asteroid.rotation = randint(0, 360)
         asteroids.append(new_asteroid)
     return asteroids
--- game/version1/game/resources.py
+++ game/version1/game/resources.py
@@ -1,22 +1,24 @@
-import pyglet
+from pyglet.resource import image
+import pyglet.resource as resource
+from pyglet.image import AbstractImage
 
 
-def center_image(image):
+def center_image(img: AbstractImage):
     """Sets an image's anchor point to its center"""
-    image.anchor_x = image.width / 2
-    image.anchor_y = image.height / 2
+    img.anchor_x = int(img.width / 2)
+    img.anchor_y = int(img.height / 2)
 
 
 # Tell pyglet where to find the resources
-pyglet.resource.path = ['../resources']
-pyglet.resource.reindex()
+resource.path = ["../resources"]
+resource.reindex()
 
 # Load the three main resources and get them to draw centered
-player_image = pyglet.resource.image("player.png")
+player_image = image("player.png")
 center_image(player_image)
 
-bullet_image = pyglet.resource.image("bullet.png")
+bullet_image = image("bullet.png")
 center_image(bullet_image)
 
-asteroid_image = pyglet.resource.image("asteroid.png")
+asteroid_image = image("asteroid.png")
 center_image(asteroid_image)
