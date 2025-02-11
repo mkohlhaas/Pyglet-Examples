d9cac75b86e21eb7352bbde034939828df5e0b2d
Hans-J. Schmid <knock@myopendoor.de>
2025-02-11 05:54:09 +0100

    gui

--- gui/widgets.py
+++ gui/widgets.py
@@ -1,8 +1,15 @@
-import pyglet
+#!/usr/bin/env python
 
-window = pyglet.window.Window(540, 500, caption="Widget Example")
-batch = pyglet.graphics.Batch()
-pyglet.gl.glClearColor(0.8, 0.8, 0.8, 1.0)
+from pyglet import app, gl
+from pyglet.graphics import Batch
+from pyglet.gui import Frame, PushButton, Slider, TextEntry, ToggleButton
+from pyglet.resource import image
+from pyglet.text import Label
+from pyglet.window import Window
+
+window = Window(540, 500, caption="Widget Example")
+batch = Batch()
+gl.glClearColor(0.8, 0.8, 0.8, 1.0)
 
 
 @window.event
@@ -15,72 +22,113 @@ def on_draw():
 # load resources to use for Widgets:
 ####################################
 
-unpressed = pyglet.resource.image('button_unpressed.png')
-pressed = pyglet.resource.image('button_pressed.png')
-hover = pyglet.resource.image('button_hover.png')
-bar = pyglet.resource.image('bar.png')
-knob = pyglet.resource.image('knob.png')
+unpressed = image("button_unpressed.png")
+pressed = image("button_pressed.png")
+hover = image("button_hover.png")
+bar = image("bar.png")
+knob = image("knob.png")
 
 
 ######################################
 # Create some event handler functions:
 ######################################
 
-def slider_handler(widget, value):
+
+def slider_handler(_widget, value):
     slider_label.text = f"Slider Value: {round(value, 1)}"
 
 
-def toggle_button_handler(widget, value):
+def toggle_button_handler(_widget, value):
     toggle_label.text = f"Toggle Button: {value}"
 
 
-def push_button_handler(widget):
+def push_button_handler(_widget):
     push_label.text = "Push Button: True"
 
 
-def release_button_handler(widget):
+def release_button_handler(_widget):
     push_label.text = "Push Button: False"
 
 
-def text_entry_handler(widget, text):
+def text_entry_handler(_widget, text):
     text_entry_label.text = f"Text: {text}"
 
 
-###############################
-# Create some Widget instances:
-###############################
-
 # A Frame instance to hold all widgets, and provide spacial
 # hashing to avoid sending all the Window events to every widget:
-frame = pyglet.gui.Frame(window, order=4)
+frame = Frame(window, order=4)
 
 
-togglebutton = pyglet.gui.ToggleButton(100, 400, pressed=pressed, unpressed=unpressed, hover=hover, batch=batch)
-togglebutton.set_handler('on_toggle', toggle_button_handler)
+togglebutton = ToggleButton(
+    100,
+    400,
+    pressed=pressed,
+    unpressed=unpressed,
+    hover=hover,
+    batch=batch,
+)
+togglebutton.set_handler("on_toggle", toggle_button_handler)
 frame.add_widget(togglebutton)
-toggle_label = pyglet.text.Label("Toggle Button: False", x=300, y=400, batch=batch, color=(0, 0, 0, 255))
+toggle_label = Label(
+    "Toggle Button: False",
+    x=300,
+    y=400,
+    batch=batch,
+    color=(0, 0, 0, 255),
+)
 
 
-pushbutton = pyglet.gui.PushButton(100, 300, pressed=pressed, unpressed=unpressed, hover=hover, batch=batch)
-pushbutton.set_handler('on_press', push_button_handler)
-pushbutton.set_handler('on_release', release_button_handler)
+pushbutton = PushButton(
+    100,
+    300,
+    pressed=pressed,
+    unpressed=unpressed,
+    hover=hover,
+    batch=batch,
+)
+pushbutton.set_handler("on_press", push_button_handler)
+pushbutton.set_handler("on_release", release_button_handler)
 frame.add_widget(pushbutton)
-push_label = pyglet.text.Label("Push Button: False", x=300, y=300, batch=batch, color=(0, 0, 0, 255))
+push_label = Label(
+    "Push Button: False",
+    x=300,
+    y=300,
+    batch=batch,
+    color=(0, 0, 0, 255),
+)
 
 
-slider = pyglet.gui.Slider(100, 200, bar, knob, edge=5, batch=batch)
-slider.set_handler('on_change', slider_handler)
+slider = Slider(100, 200, bar, knob, edge=5, batch=batch)
+slider.set_handler("on_change", slider_handler)
 frame.add_widget(slider)
-slider_label = pyglet.text.Label("Slider Value: 0.0", x=300, y=200, batch=batch, color=(0, 0, 0, 255))
+slider_label = Label(
+    "Slider Value: 0.0",
+    x=300,
+    y=200,
+    batch=batch,
+    color=(0, 0, 0, 255),
+)
 
 
 # This Widget is not added to the Frame. Because it is sensitive
 # to drag-and-select events falling outside the Frame's spatial hash,
 # it's best to let it handle Window events directly.
-text_entry = pyglet.gui.TextEntry("Enter Your Name", 100, 100, 150, batch=batch)
+text_entry = TextEntry(
+    "Enter Your Name",
+    100,
+    100,
+    150,
+    batch=batch,
+)
 window.push_handlers(text_entry)
-text_entry.set_handler('on_commit', text_entry_handler)
-text_entry_label = pyglet.text.Label("Text: None", x=300, y=100, batch=batch, color=(0, 0, 0, 255))
+text_entry.set_handler("on_commit", text_entry_handler)
+text_entry_label = Label(
+    "Text: None",
+    x=300,
+    y=100,
+    batch=batch,
+    color=(0, 0, 0, 255),
+)
 
 
-pyglet.app.run()
+app.run()
